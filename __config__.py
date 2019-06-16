1dcad5711b248cca64db99612bfdb67c947f0c61
Efthymios Tzinis <etzinis2@illinois.edu>
2019-06-15 20:19:00 -0500

    Adding paths of the preprocessed dataset paths

--- __config__.py
+++ __config__.py
@@ -2,5 +2,12 @@ import os
 
 WSJ0_MIX_2_8K_PATH = '/mnt/data/wsj0-mix/2speakers/wav8k'
 WSJ0_MIX_2_16K_PATH = '/mnt/data/wsj0-mix/2speakers/wav16k'
+WSJ_MIX_2_8K_PREPROCESSED_TRAIN_P = \
+    '/mnt/nvme/wsj0_mix_preprocessed/wsj0_2mix_8.0k_4.0s_min_preprocessed/tr'
+WSJ_MIX_2_8K_PREPROCESSED_EVAL_P = \
+    '/mnt/nvme/wsj0_mix_preprocessed/wsj0_2mix_8.0k_4.0s_min_preprocessed/cv'
+WSJ_MIX_2_8K_PREPROCESSED_TEST_P = \
+    '/mnt/nvme/wsj0_mix_preprocessed/wsj0_2mix_8.0k_4.0s_min_preprocessed/tt'
+API_KEY = '66xB8zZUV1IVstsauUe6royLR'
 
 
