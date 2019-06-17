b329b35dd684e8ea6e459447ec2780cf78e3a845
Efthymios Tzinis <etzinis2@illinois.edu>
2019-06-16 22:42:07 -0500

    Run tasnet v1 draft version working with cometml

--- two_step_mask_learning/dnn/experiments/run_tasnet_v1.py
+++ two_step_mask_learning/dnn/experiments/run_tasnet_v1.py
@@ -14,39 +14,47 @@ from __config__ import API_KEY
 from comet_ml import Experiment
 
 import torch
-import torch.nn as nn
-import torchvision.datasets as dsets
-import torchvision.transforms as transforms
-from torch.autograd import Variable
+from tqdm import tqdm
+from pprint import pprint
 import two_step_mask_learning.dnn.dataset_loader.torch_dataloader as dataloader
 from __config__ import WSJ_MIX_2_8K_PREPROCESSED_EVAL_P, \
     WSJ_MIX_2_8K_PREPROCESSED_TEST_P, WSJ_MIX_2_8K_PREPROCESSED_TRAIN_P
 import two_step_mask_learning.dnn.losses.sisdr as sisdr_lib
 import two_step_mask_learning.dnn.losses.norm as norm_lib
+import two_step_mask_learning.dnn.models.conv_tasnet_wrapper as tasnet_wrapper
+import two_step_mask_learning.dnn.utils.cometml_loss_report as cometml_report
 
 
 hparams = {
-    "sequence_length": 28,
-    "input_size": 28,
-    "hidden_size": 128,
-    "num_layers": 2,
-    "num_classes": 10,
-    "bs": 16,
-    "n_jobs":3,
-    "tr_get_top": 64,
-    "val_get_top": 64,
-    "num_epochs": 3,
-    "learning_rate": 0.01
+    "experiment_name": 'alvanos',
+    "R": 4,
+    "P": 3,
+    "X": 8,
+    "embedding_width": 1,
+    "bs": 7,
+    "n_jobs": 3,
+    "tr_get_top": 80,
+    "val_get_top": 80,
+    "cuda_devs": ['cuda:0'],
+    "n_epochs": 500,
+    "learning_rate": 0.001
 }
 
 
-# experiment = Experiment(API_KEY, project_name="first_tasnet_wsj02mix")
-# experiment.log_parameters(hyper_params)
+experiment = Experiment(API_KEY,
+                        project_name="first_tasnet_wsj02mix")
+experiment.log_parameters(hparams)
 
+experiment.add_tag('yolarelis')
+if hparams['experiment_name'] is not None:
+    experiment.set_name(hparams['experiment_name'])
+
+# these parameters should be automatically inferred
+in_samples = 32000
 n_sources = 2
 
 # define data loaders
-train_loader, eval_loader = dataloader.get_data_generators(
+train_gen, val_gen = dataloader.get_data_generators(
     [WSJ_MIX_2_8K_PREPROCESSED_TRAIN_P,
      WSJ_MIX_2_8K_PREPROCESSED_EVAL_P],
     bs=hparams['bs'], n_jobs=hparams['n_jobs'],
@@ -55,17 +63,14 @@ train_loader, eval_loader = dataloader.get_data_generators(
 )
 
 # define the losses that are going to be used
-recon_loss_name, recon_loss = (
-    'tr_SISDR',
+back_loss_tr_loss_name, back_loss_tr_loss = (
+    'tr_back_loss_SISDR',
     sisdr_lib.PermInvariantSISDR(batch_size=hparams['bs'],
                                  n_sources=n_sources,
                                  zero_mean=False,
                                  backward_loss=True))
 
 val_losses = dict([
-    ('val_L1', norm_lib.PermInvariantNorm(batch_size=hparams['bs'],
-                                          zero_mean=False,
-                                          n_sources=n_sources)),
     ('val_SISDR', sisdr_lib.PermInvariantSISDR(batch_size=hparams['bs'],
                                                n_sources=n_sources,
                                                zero_mean=True,
@@ -77,98 +82,90 @@ val_losses = dict([
   ])
 
 train_losses = dict([
-    ('tr_L1', norm_lib.PermInvariantNorm(batch_size=hparams['bs'],
-                                         zero_mean=False,
-                                         n_sources=n_sources)),
-    ('tr_SISDR', sisdr_lib.PermInvariantSISDR(batch_size=hparams['bs'],
-                                              n_sources=n_sources,
-                                              zero_mean=True,
-                                              backward_loss=False)),
     ('tr_SISDR_AE', sisdr_lib.PermInvariantSISDR(batch_size=hparams['bs'],
                                                  n_sources=1,
                                                  zero_mean=True,
                                                  backward_loss=False))])
 
-#
-# # RNN Model (Many-to-One)
-# class RNN(nn.Module):
-#     def __init__(self, input_size, hidden_size, num_layers, num_classes):
-#         super(RNN, self).__init__()
-#         self.hidden_size = hidden_size
-#         self.num_layers = num_layers
-#         self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
-#         self.fc = nn.Linear(hidden_size, num_classes)
-#
-#     def forward(self, x):
-#         # Set initial states
-#         h0 = Variable(torch.zeros(self.num_layers, x.size(0), self.hidden_size))
-#         c0 = Variable(torch.zeros(self.num_layers, x.size(0), self.hidden_size))
-#
-#         # Forward propagate RNN
-#         out, _ = self.lstm(x, (h0, c0))
-#
-#         # Decode hidden state of last time step
-#         out = self.fc(out[:, -1, :])
-#         return out
-#
-#
-#
-# rnn = RNN(hyper_params['input_size'],
-#           hyper_params['hidden_size'],
-#           hyper_params['num_layers'],
-#           hyper_params['num_classes'])
-#
-# # Loss and Optimizer
-# criterion = nn.CrossEntropyLoss()
-# optimizer = torch.optim.Adam(rnn.parameters(), lr=hyper_params['learning_rate'])
-#
-# # Train the Model
-#
-# with experiment.train():
-#
-#     step = 0
-#     for epoch in range(hyper_params['num_epochs']):
-#         experiment.log_current_epoch(epoch)
-#         correct = 0
-#         total = 0
-#         for i, (images, labels) in enumerate(train_loader):
-#             images = Variable(images.view(-1, hyper_params['sequence_length'],
-#                                           hyper_params['input_size']))
-#             labels = Variable(labels)
-#
-#             # Forward + Backward + Optimize
-#             optimizer.zero_grad()
-#             outputs = rnn(images)
-#             loss = criterion(outputs, labels)
-#             loss.backward()
-#             optimizer.step()
-#
-#             # Compute train accuracy
-#             _, predicted = torch.max(outputs.data, 1)
-#             total += labels.size(0)
-#             correct += float((predicted == labels.data).sum())
-#
-#             # Log accuracy to Comet.ml
-#             experiment.log_metric("accuracy", correct / total, step=step)
-#             step += 1
-#
-#             if (i + 1) % 100 == 0:
-#                 print('Epoch [%d/%d], Step [%d/%d], Loss: %.4f'
-#                       % (epoch + 1, hyper_params['num_epochs'], i + 1,
-#                          len(train_dataset) // hyper_params['batch_size'],
-#                          loss.data.item()))
-# with experiment.test():
-#     # Test the Model
-#     correct = 0
-#     total = 0
-#     for images, labels in test_loader:
-#         images = Variable(images.view(-1, hyper_params['sequence_length'],
-#                                       hyper_params['input_size']))
-#         outputs = rnn(images)
-#         _, predicted = torch.max(outputs.data, 1)
-#         total += labels.size(0)
-#         correct += float((predicted == labels).sum())
-#
-#     experiment.log_metric("accuracy", correct / total)
-#     print('Test Accuracy of the model on the 10000 test images: %d %%'
-#           % (100 * correct / total))
+os.environ['CUDA_VISIBLE_DEVICES'] = ','.join([cad[-1]
+                                               for cad in hparams['cuda_devs']])
+
+
+model = tasnet_wrapper.TasNetFrontendsWrapper(
+    in_samples,
+    pretrained_encoder=None,
+    pretrained_decoder=None,
+    n_sources=n_sources,
+    embedding_width=hparams['embedding_width'],
+    P=hparams['P'],
+    R=hparams['R'],
+    X=hparams['X'])
+
+model.cuda()
+opt = torch.optim.Adam(model.parameters(), lr=hparams['learning_rate'])
+all_losses = [back_loss_tr_loss_name] + \
+             [k for k in sorted(train_losses.keys())] + \
+             [k for k in sorted(val_losses.keys())]
+
+res_dic = {}
+for loss_name in all_losses:
+    res_dic[loss_name] = {'mean': 0., 'std': 0., 'acc': []}
+
+tr_step = 0
+val_step = 0
+for i in range(hparams['n_epochs']):
+    model.train()
+
+    for data in tqdm(train_gen, desc='Training'):
+        opt.zero_grad()
+        m1wavs = data[0].cuda()
+        clean_wavs = data[-1].cuda()
+
+        rec_sources_wavs = model.infer_source_signals(m1wavs)
+        l = back_loss_tr_loss(rec_sources_wavs, clean_wavs)
+        l.backward()
+        opt.step()
+        res_dic[back_loss_tr_loss_name]['acc'].append(l.item())
+    tr_step += 1
+
+    if val_gen is not None:
+        model.eval()
+        with torch.no_grad():
+            for data in tqdm(val_gen, desc='Validation'):
+                m1wavs = data[0].cuda()
+                clean_wavs = data[-1].cuda()
+
+                for loss_name, loss_func in val_losses.items():
+                    if 'AE' in loss_name:
+                        AE_rec_mixture = model.AE_recontruction(m1wavs)
+                        l = loss_func(AE_rec_mixture, m1wavs)
+                    else:
+                        rec_wavs = model.infer_source_signals(m1wavs)
+                        l = loss_func(rec_wavs, clean_wavs)
+                    res_dic[loss_name]['acc'].append(l.item())
+
+        val_step += 1
+
+    if train_losses.values():
+        model.eval()
+        with torch.no_grad():
+            for data in tqdm(train_gen, desc='Train Validation'):
+                m1wavs = data[0].cuda()
+                clean_wavs = data[-1].cuda()
+
+                for loss_name, loss_func in val_losses.items():
+                    if 'AE' in loss_name:
+                        AE_rec_mixture = model.AE_recontruction(m1wavs)
+                        l = loss_func(AE_rec_mixture, m1wavs)
+                    else:
+                        rec_wavs = model.infer_source_signals(m1wavs)
+                        l = loss_func(rec_wavs, clean_wavs)
+                    res_dic[loss_name]['acc'].append(l.item())
+
+    res_dic = cometml_report.report_losses_mean_and_std(res_dic,
+                                                        experiment,
+                                                        tr_step,
+                                                        val_step)
+    for loss_name in res_dic:
+        res_dic[loss_name]['acc'] = []
+    pprint(res_dic)
