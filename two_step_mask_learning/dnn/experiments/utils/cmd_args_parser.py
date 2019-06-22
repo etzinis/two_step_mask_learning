1919fceeeb986afab47622b5da2dec5112df73bc
Efthymios Tzinis <etzinis2@illinois.edu>
2019-06-22 17:01:13 -0500

    Arg parser for comet ml experiment

---
+++ two_step_mask_learning/dnn/experiments/utils/cmd_args_parser.py
@@ -0,0 +1,91 @@
+"""!
+@brief Experiment Argument Parser
+
+@author Efthymios Tzinis {etzinis2@illinois.edu}
+@copyright University of Illinois at Urbana-Champaign
+"""
+
+import argparse
+
+
+def get_args():
+    """! Command line parser """
+    parser = argparse.ArgumentParser(
+        description='CometML Experiment Argument Parser')
+    parser.add_argument("--train", type=str,
+                        help="Training dataset",
+                        default='WSJ2MIX8K',
+                        choices=['WSJ2MIX8K', 'TIMITMF8K'])
+    parser.add_argument("--val", type=str,
+                        help="Validation dataset",
+                        default='WSJ2MIX8K',
+                        choices=['WSJ2MIX8K', 'TIMITMF8K'])
+    parser.add_argument("--n_train", type=int,
+                        help="""Reduce the number of training 
+                            samples to this number.""", default=None)
+    parser.add_argument("--n_val", type=int,
+                        help="""Reduce the number of evaluation 
+                            samples to this number.""", default=None)
+    parser.add_argument("-ri", "--return_items", type=str, nargs='+',
+                        help="""A list of elements that this 
+                        dataloader should return. See available 
+                        choices which are based on the saved data 
+                        names which are available. There is no type 
+                        checking in this return argument.""",
+                        default=['mixture_wav', 'clean_sources_wavs'],
+                        choices=['mixture_wav', 'clean_sources_wavs'])
+    parser.add_argument("-tags", "--cometml_tags", type=str,
+                        nargs="+", help="""A list of tags for the cometml 
+                        experiment.""",
+                        default=[])
+    parser.add_argument("--experiment_name", type=str,
+                        help="""Name of current experiment""",
+                        default=None)
+    parser.add_argument("--project_name", type=str,
+                        help="""Name of current experiment""",
+                        default="first_wsj02mix")
+
+    # device params
+    parser.add_argument("-cad", "--cuda_available_devices", type=str,
+                        nargs="+",
+                        help="""A list of Cuda IDs that would be 
+                        available for running this experiment""",
+                        default=['0'],
+                        choices=['0', '1', '2', '3'])
+    # model params
+    parser.add_argument("-N", "--n_basis", type=int,
+                        help="Dim of encoded representation",
+                        default=256)
+    parser.add_argument("-B", "--tasnet_B", type=int,
+                        help="Number of dimensions in bottleneck.",
+                        default=256)
+    parser.add_argument("-H", "--tasnet_H", type=int,
+                        help="Number of channels in convd.",
+                        default=512)
+    parser.add_argument("-L", "--n_kernel", type=int,
+                        help="Length of encoding convolutional filters",
+                        default=20)
+    parser.add_argument("-P", "--tasnet_P", type=int,
+                        help="Length of filters in convolution blocks",
+                        default=3)
+    parser.add_argument("-X", "--tasnet_X", type=int,
+                        help="Number of convolutional blocks in each repeat",
+                        default=8)
+    parser.add_argument("-R", "--tasnet_R", type=int,
+                        help="Number of repeats of TN Blocks",
+                        default=4)
+    # training params
+    parser.add_argument("-bs", "--batch_size", type=int,
+                        help="""The number of samples in each batch. 
+                            Warning: Cannot be less than the number of 
+                            the validation samples""", default=4)
+    parser.add_argument("--n_jobs", type=int,
+                        help="""The number of cpu workers for 
+                            loading the data, etc.""", default=4)
+    parser.add_argument("--n_epochs", type=int,
+                        help="""The number of epochs that the 
+                        experiment should run""", default=50)
+    parser.add_argument("-lr", "--learning_rate", type=float,
+                        help="""Initial Learning rate""", default=1e-2)
+
+    return parser.parse_args()
