f91848815ed21818696fc32e9a164741512eb11f
Efthymios Tzinis <etzinis2@illinois.edu>
2019-06-22 17:03:46 -0500

    Tasnet fully configurable cometml experiment script ready to run

--- two_step_mask_learning/dnn/experiments/run_tasnet_v1.py
+++ two_step_mask_learning/dnn/experiments/run_tasnet_v1.py
@@ -19,47 +19,79 @@ from pprint import pprint
 import two_step_mask_learning.dnn.dataset_loader.torch_dataloader as dataloader
 from __config__ import WSJ_MIX_2_8K_PREPROCESSED_EVAL_P, \
     WSJ_MIX_2_8K_PREPROCESSED_TEST_P, WSJ_MIX_2_8K_PREPROCESSED_TRAIN_P
+from __config__ import TIMIT_MIX_2_8K_PREPROCESSED_EVAL_P, \
+    TIMIT_MIX_2_8K_PREPROCESSED_TEST_P, TIMIT_MIX_2_8K_PREPROCESSED_TRAIN_P
 import two_step_mask_learning.dnn.losses.sisdr as sisdr_lib
 import two_step_mask_learning.dnn.losses.norm as norm_lib
 import two_step_mask_learning.dnn.models.conv_tasnet_wrapper as tasnet_wrapper
 import two_step_mask_learning.dnn.utils.cometml_loss_report as cometml_report
+import two_step_mask_learning.dnn.experiments.utils.cmd_args_parser as parser
+
+
+args = parser.get_args()
 
 
 hparams = {
-    "experiment_name": 'alvanos',
-    "R": 4,
-    "P": 3,
-    "X": 8,
-    "embedding_width": 1,
-    "bs": 7,
-    "n_jobs": 3,
-    "tr_get_top": 80,
-    "val_get_top": 80,
-    "cuda_devs": ['cuda:0'],
-    "n_epochs": 500,
-    "learning_rate": 0.001
+    "train_dataset": args.train,
+    "val_dataset": args.val,
+    "experiment_name": args.experiment_name,
+    "project_name": args.project_name,
+    "R": args.tasnet_R,
+    "P": args.tasnet_P,
+    "X": args.tasnet_X,
+    "B": args.tasnet_B,
+    "H": args.tasnet_H,
+    "n_kernel": args.n_kernel,
+    "n_basis": args.n_basis,
+    "bs": args.batch_size,
+    "n_jobs": args.n_jobs,
+    "tr_get_top": args.n_train,
+    "val_get_top": args.n_val,
+    "cuda_devs": args.cuda_available_devices,
+    "n_epochs": args.n_epochs,
+    "learning_rate": args.learning_rate,
+    "return_items": args.return_items,
+    "tags": args.cometml_tags
 }
 
+if (hparams['train_dataset'] == 'WSJ2MIX8K' and
+    hparams['val_dataset'] == 'WSJ2MIX8K'):
+    in_samples = 32000
+    n_sources = 2
+    hparams['train_dataset_path'] = WSJ_MIX_2_8K_PREPROCESSED_TRAIN_P
+    hparams['val_dataset_path'] = WSJ_MIX_2_8K_PREPROCESSED_EVAL_P
+elif(hparams['train_dataset'] == 'TIMITMF8K' and
+     hparams['val_dataset'] == 'TIMITMF8K'):
+    n_sources = 2
+    in_samples = 16000
+    hparams['train_dataset_path'] = TIMIT_MIX_2_8K_PREPROCESSED_TRAIN_P
+    hparams['val_dataset_path'] = TIMIT_MIX_2_8K_PREPROCESSED_EVAL_P
+    hparams['return_items'] = return_items=['mic1_wav_downsampled',
+                                            'clean_sources_wavs_downsampled']
+else:
+    raise NotImplementedError('Datasets: {}, {} are not available'
+                              ''.format(hparams['train_dataset'],
+                                        hparams['val_dataset']))
 
 experiment = Experiment(API_KEY,
-                        project_name="first_tasnet_wsj02mix")
+                        project_name=hparams['project_name'])
 experiment.log_parameters(hparams)
 
-experiment.add_tag('yolarelis')
+experiment_name = '_'.join(hparams['tags'])
+for tag in hparams['tags']:
+    experiment.add_tag(tag)
+
 if hparams['experiment_name'] is not None:
     experiment.set_name(hparams['experiment_name'])
-
-# these parameters should be automatically inferred
-in_samples = 32000
-n_sources = 2
+else:
+    experiment.set_name(experiment_name)
 
 # define data loaders
 train_gen, val_gen = dataloader.get_data_generators(
-    [WSJ_MIX_2_8K_PREPROCESSED_TRAIN_P,
-     WSJ_MIX_2_8K_PREPROCESSED_EVAL_P],
+    [hparams['train_dataset_path'], hparams['val_dataset_path']],
     bs=hparams['bs'], n_jobs=hparams['n_jobs'],
     get_top=[hparams['tr_get_top'], hparams['val_get_top']],
-    return_items=['mixture_wav', 'clean_sources_wavs']
+    return_items=hparams['return_items']
 )
 
 # define the losses that are going to be used
@@ -67,7 +99,7 @@ back_loss_tr_loss_name, back_loss_tr_loss = (
     'tr_back_loss_SISDR',
     sisdr_lib.PermInvariantSISDR(batch_size=hparams['bs'],
                                  n_sources=n_sources,
-                                 zero_mean=False,
+                                 zero_mean=True,
                                  backward_loss=True))
 
 val_losses = dict([
@@ -87,7 +119,7 @@ train_losses = dict([
                                                  zero_mean=True,
                                                  backward_loss=False))])
 
-os.environ['CUDA_VISIBLE_DEVICES'] = ','.join([cad[-1]
+os.environ['CUDA_VISIBLE_DEVICES'] = ','.join([cad
                                                for cad in hparams['cuda_devs']])
 
 
@@ -96,10 +128,19 @@ model = tasnet_wrapper.TasNetFrontendsWrapper(
     pretrained_encoder=None,
     pretrained_decoder=None,
     n_sources=n_sources,
-    embedding_width=hparams['embedding_width'],
+    B=hparams['B'],
+    H=hparams['H'],
     P=hparams['P'],
     R=hparams['R'],
-    X=hparams['X'])
+    X=hparams['X'],
+    L=hparams['n_kernel'],
+    N=hparams['n_basis'])
+
+numparams = 0
+for f in model.parameters():
+    if f.requires_grad:
+        numparams += f.numel()
+experiment.log_parameter('Parameters', numparams)
 
 model.cuda()
 opt = torch.optim.Adam(model.parameters(), lr=hparams['learning_rate'])
@@ -114,14 +155,15 @@ for loss_name in all_losses:
 tr_step = 0
 val_step = 0
 for i in range(hparams['n_epochs']):
+    print("Epoch: {}/{}".format(i+1, hparams['n_epochs']))
     model.train()
 
     for data in tqdm(train_gen, desc='Training'):
         opt.zero_grad()
-        m1wavs = data[0].cuda()
+        m1wavs = data[0].unsqueeze(1).cuda()
         clean_wavs = data[-1].cuda()
 
-        rec_sources_wavs = model.infer_source_signals(m1wavs)
+        _, rec_sources_wavs = model(m1wavs, return_wavs=True)
         l = back_loss_tr_loss(rec_sources_wavs, clean_wavs)
         l.backward()
         opt.step()
@@ -132,7 +174,7 @@ for i in range(hparams['n_epochs']):
         model.eval()
         with torch.no_grad():
             for data in tqdm(val_gen, desc='Validation'):
-                m1wavs = data[0].cuda()
+                m1wavs = data[0].unsqueeze(1).cuda()
                 clean_wavs = data[-1].cuda()
 
                 for loss_name, loss_func in val_losses.items():
@@ -150,7 +192,7 @@ for i in range(hparams['n_epochs']):
         model.eval()
         with torch.no_grad():
             for data in tqdm(train_gen, desc='Train Validation'):
-                m1wavs = data[0].cuda()
+                m1wavs = data[0].unsqueeze(1).cuda()
                 clean_wavs = data[-1].cuda()
 
                 for loss_name, loss_func in val_losses.items():
