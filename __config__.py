b50ba715badbf6b5e44ecbbfca2f98bc36d480bd
Efthymios Tzinis <etzinis2@illinois.edu>
2019-07-05 18:43:52 -0500

    Adding extra path parameters for saving models and loading from

--- __config__.py
+++ __config__.py
@@ -1,4 +1,5 @@
-import os 
+import os
+
 
 WSJ0_MIX_2_8K_PATH = '/mnt/data/wsj0-mix/2speakers/wav8k'
 WSJ0_MIX_2_16K_PATH = '/mnt/data/wsj0-mix/2speakers/wav16k'
@@ -25,5 +26,12 @@ WSJ_MIX_2_8K_PREPROCESSED_EVAL_PAD_P = \
     '/mnt/nvme/wsj0_mix_preprocessed/wsj0_2mix_8.0k_4.0s_max_preprocessed/cv'
 WSJ_MIX_2_8K_PREPROCESSED_TEST_PAD_P = \
     '/mnt/nvme/wsj0_mix_preprocessed/wsj0_2mix_8.0k_4.0s_max_preprocessed/tt'
+
+AFE_WSJ_MIX_2_8K = '/home/thymios/afes/min/'
+AFE_WSJ_MIX_2_8K_PAD = '/home/thymios/afes/pad/'
+AFE_WSJ_MIX_2_8K_NORMPAD = '/home/thymios/afes/norm_pad/'
+TNMASK_WSJ_MIX_2_8K = '/home/thymios/tn_mask/min/'
+TNMASK_WSJ_MIX_2_8K_PAD = '/home/thymios/tn_mask/pad/'
+TNMASK_WSJ_MIX_2_8K_NORMPAD = '/home/thymios/tn_mask/norm_pad/'
 
 API_KEY = '66xB8zZUV1IVstsauUe6royLR'
