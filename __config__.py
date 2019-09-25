70eed7389c4dd89fd5e53cb996323871a071b73b
Efthymios Tzinis <etzinis2@illinois.edu>
2019-09-25 14:45:35 -0500

    Updated paths for datasets

--- __config__.py
+++ __config__.py
@@ -4,34 +4,41 @@ import os
 WSJ0_MIX_2_8K_PATH = '/mnt/data/wsj0-mix/2speakers/wav8k'
 WSJ0_MIX_2_16K_PATH = '/mnt/data/wsj0-mix/2speakers/wav16k'
 WSJ_MIX_2_8K_PREPROCESSED_TRAIN_P = \
-    '/mnt/nvme/wsj0_mix_preprocessed/wsj0_2mix_8.0k_4.0s_min_preprocessed/tr'
+    '/mnt/data/wsj0_mix_preprocessed/wsj0_2mix_8.0k_4.0s_min_preprocessed/tr'
 WSJ_MIX_2_8K_PREPROCESSED_EVAL_P = \
-    '/mnt/nvme/wsj0_mix_preprocessed/wsj0_2mix_8.0k_4.0s_min_preprocessed/cv'
+    '/mnt/data/wsj0_mix_preprocessed/wsj0_2mix_8.0k_4.0s_min_preprocessed/cv'
 WSJ_MIX_2_8K_PREPROCESSED_TEST_P = \
-    '/mnt/nvme/wsj0_mix_preprocessed/wsj0_2mix_8.0k_4.0s_min_preprocessed/tt'
+    '/mnt/data/wsj0_mix_preprocessed/wsj0_2mix_8.0k_4.0s_min_preprocessed/tt'
 
 TIMIT_MIX_2_8K_PREPROCESSED_TRAIN_P = \
-    '/mnt/nvme/end2end_unsupervised_holder' \
+    '/mnt/data/end2end_unsupervised_holder' \
     '/timit_10000_1000_1000_2_fm_random_taus_delays/train'
 TIMIT_MIX_2_8K_PREPROCESSED_EVAL_P = \
-    '/mnt/nvme/end2end_unsupervised_holder' \
+    '/mnt/data/end2end_unsupervised_holder' \
     '/timit_10000_1000_1000_2_fm_random_taus_delays/val'
 TIMIT_MIX_2_8K_PREPROCESSED_TEST_P = \
-    '/mnt/nvme/end2end_unsupervised_holder' \
+    '/mnt/data/end2end_unsupervised_holder' \
     '/timit_10000_1000_1000_2_fm_random_taus_delays/test'
 
 WSJ_MIX_2_8K_PREPROCESSED_TRAIN_PAD_P = \
-    '/mnt/nvme/wsj0_mix_preprocessed/wsj0_2mix_8.0k_4.0s_max_preprocessed/tr'
+    '/mnt/data/wsj0_mix_preprocessed/wsj0_2mix_8.0k_4.0s_max_preprocessed/tr'
 WSJ_MIX_2_8K_PREPROCESSED_EVAL_PAD_P = \
-    '/mnt/nvme/wsj0_mix_preprocessed/wsj0_2mix_8.0k_4.0s_max_preprocessed/cv'
+    '/mnt/data/wsj0_mix_preprocessed/wsj0_2mix_8.0k_4.0s_max_preprocessed/cv'
 WSJ_MIX_2_8K_PREPROCESSED_TEST_PAD_P = \
-    '/mnt/nvme/wsj0_mix_preprocessed/wsj0_2mix_8.0k_4.0s_max_preprocessed/tt'
+    '/mnt/data/wsj0_mix_preprocessed/wsj0_2mix_8.0k_4.0s_max_preprocessed/tt'
+
+WSJ_MIX_HIERARCHICAL_P = '/mnt/data/hierarchical_sound_datasets/WSJ0_mix_partitioned/'
+ESC50_HIERARCHICAL_P = '/mnt/data/hierarchical_sound_datasets/ESC50_partitioned/'
 
 AFE_WSJ_MIX_2_8K = '/home/thymios/afes/min/'
 AFE_WSJ_MIX_2_8K_PAD = '/home/thymios/afes/pad/'
 AFE_WSJ_MIX_2_8K_NORMPAD = '/home/thymios/afes/norm_pad/'
+AFE_AUGMENTED = '/home/thymios/afes/augmented/'
+
 TNMASK_WSJ_MIX_2_8K = '/home/thymios/tn_mask/min/'
 TNMASK_WSJ_MIX_2_8K_PAD = '/home/thymios/tn_mask/pad/'
 TNMASK_WSJ_MIX_2_8K_NORMPAD = '/home/thymios/tn_mask/norm_pad/'
+TNMASK_AUGMENTED = '/home/thymios/tn_mask/augmented/'
+
 
 API_KEY = '66xB8zZUV1IVstsauUe6royLR'
