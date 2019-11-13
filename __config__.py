8eb626f36663d47d79810a25fdb1334200018d66
Efthymios Tzinis <etzinis2@illinois.edu>
2019-11-13 14:01:44 -0600

    Making more configurable paths for WSJ dataset

--- __config__.py
+++ __config__.py
@@ -3,12 +3,18 @@ import os
 
 WSJ0_MIX_2_8K_PATH = '/mnt/data/wsj0-mix/2speakers/wav8k'
 WSJ0_MIX_2_16K_PATH = '/mnt/data/wsj0-mix/2speakers/wav16k'
-WSJ_MIX_2_8K_PREPROCESSED_TRAIN_P = \
-    '/mnt/data/wsj0_mix_preprocessed/wsj0_2mix_8.0k_4.0s_min_preprocessed/tr'
-WSJ_MIX_2_8K_PREPROCESSED_EVAL_P = \
-    '/mnt/data/wsj0_mix_preprocessed/wsj0_2mix_8.0k_4.0s_min_preprocessed/cv'
-WSJ_MIX_2_8K_PREPROCESSED_TEST_P = \
-    '/mnt/data/wsj0_mix_preprocessed/wsj0_2mix_8.0k_4.0s_min_preprocessed/tt'
+
+WSJ0_MIX_2_8K_PREPROCESSED_BASE_P = '/mnt/nvme/wsj0_mix_preprocessed'
+WSJ0_MIX_2_8K_PREPROCESSED_MIN_BASE_P = os.path.join(
+    WSJ0_MIX_2_8K_PREPROCESSED_BASE_P,
+    'wsj0_2mix_8.0k_4.0s_min_preprocessed')
+WSJ0_MIX_2_8K_PREPROCESSED_MAX_BASE_P = os.path.join(
+    WSJ0_MIX_2_8K_PREPROCESSED_BASE_P,
+    'wsj0_2mix_8.0k_4.0s_max_preprocessed')
+
+WSJ_MIX_2_8K_PREPROCESSED_TRAIN_P = os.path.join(WSJ0_MIX_2_8K_PREPROCESSED_MIN_BASE_P, 'tr')
+WSJ_MIX_2_8K_PREPROCESSED_EVAL_P = os.path.join(WSJ0_MIX_2_8K_PREPROCESSED_MIN_BASE_P, 'cv')
+WSJ_MIX_2_8K_PREPROCESSED_TEST_P = os.path.join(WSJ0_MIX_2_8K_PREPROCESSED_MIN_BASE_P, 'tt')
 
 TIMIT_MIX_2_8K_PREPROCESSED_TRAIN_P = \
     '/mnt/data/end2end_unsupervised_holder' \
@@ -20,12 +26,9 @@ TIMIT_MIX_2_8K_PREPROCESSED_TEST_P = \
     '/mnt/data/end2end_unsupervised_holder' \
     '/timit_10000_1000_1000_2_fm_random_taus_delays/test'
 
-WSJ_MIX_2_8K_PREPROCESSED_TRAIN_PAD_P = \
-    '/mnt/data/wsj0_mix_preprocessed/wsj0_2mix_8.0k_4.0s_max_preprocessed/tr'
-WSJ_MIX_2_8K_PREPROCESSED_EVAL_PAD_P = \
-    '/mnt/data/wsj0_mix_preprocessed/wsj0_2mix_8.0k_4.0s_max_preprocessed/cv'
-WSJ_MIX_2_8K_PREPROCESSED_TEST_PAD_P = \
-    '/mnt/data/wsj0_mix_preprocessed/wsj0_2mix_8.0k_4.0s_max_preprocessed/tt'
+WSJ_MIX_2_8K_PREPROCESSED_TRAIN_PAD_P = os.path.join(WSJ0_MIX_2_8K_PREPROCESSED_MAX_BASE_P, 'tr')
+WSJ_MIX_2_8K_PREPROCESSED_EVAL_PAD_P = os.path.join(WSJ0_MIX_2_8K_PREPROCESSED_MAX_BASE_P, 'cv')
+WSJ_MIX_2_8K_PREPROCESSED_TEST_PAD_P = os.path.join(WSJ0_MIX_2_8K_PREPROCESSED_MAX_BASE_P, 'tt')
 
 WSJ_MIX_HIERARCHICAL_P = '/mnt/data/hierarchical_sound_datasets/WSJ0_mix_partitioned/'
 ESC50_HIERARCHICAL_P = '/mnt/data/hierarchical_sound_datasets/ESC50_partitioned/'
